99109b96f5a8e57cd2979374a8098120e90138e6
Szalai Szabolcs <sz.szalai16@gmail.com>
2021-11-18 11:55:44 +0100

    benne van-e

--- src/hu/petrik/Morze.java
+++ src/hu/petrik/Morze.java
@@ -1,10 +1,12 @@
 package hu.petrik;
 
 import java.io.BufferedReader;
+import java.io.Console;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Morze {
     private HashMap<String, String> betubol;
@@ -15,7 +17,8 @@ public class Morze {
         this.morzebol = new HashMap<>();
         beolvas();
         //kiir();
-        System.out.printf("3. feladat: A morze abc %d db karakter kódját tartalmazza", betubol.size());
+        System.out.printf("3. feladat: A morze abc %d db karakter kódját tartalmazza\n", betubol.size());
+        benneVanE();
     }
 
     private void beolvas(){
@@ -46,6 +49,17 @@ public class Morze {
         System.out.println();
         for (Map.Entry<String, String> entry: betubol.entrySet()) {
             System.out.printf("%-8s%s%n", entry.getValue(), entry.getKey());
+        }
+    }
+
+    public void benneVanE() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("4. feladat: Kérek egy karaktert: ");
+        String karakter = sc.nextLine().toUpperCase();
+        if (betubol.containsKey(karakter)) {
+            System.out.printf("\t\tA(z) %s karakter morze kódja: %s\n", karakter, betubol.get(karakter));
+        } else {
+            System.out.println("Nem található a kódtárban ilyen karakter!");
         }
     }
 }
